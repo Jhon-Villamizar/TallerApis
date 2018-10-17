e7036b977c8e52fef085f1d4ec0ee2e547b2c2bb
Camila <camila@gmail.com>
2018-10-16 21:13:52 -0500

    cambios

---
+++ TallerApi.Xamarin/TallerApi.Xamarin/Views/FacebookPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerApi.Xamarin.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FacebookPage : ContentPage
+	{
+		public FacebookPage ()
+		{
+			InitializeComponent ();
+            CargarPublicaciones();
+		}
+
+        private async void CargarPublicaciones()
+        {
+            HttpClient cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("http://172.16.58.174");
+
+            var request = await cliente.GetAsync("/TallerApis/api/Publicacion");
+            if(request.IsSuccessStatusCode)
+            {
+                var responseJson = request.Content.ReadAsStringAsync();
+                var listado = JsonConvert.DeserializeObject<List<Publicacion>>(responseJson);
+            }
+        }
+    }
+}
